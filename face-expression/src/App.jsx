d16ada2f34ab633278e0351cbe3f9488b2308a8b
ravi8545 <139250843+ravi8545@users.noreply.github.com>
2026-03-22 12:44:22 +0530

    creating face expression app

---
+++ face-expression/src/App.jsx
@@ -0,0 +1,13 @@
+
+import FaceExpressionDetector from "./FaceExpressionDetector";
+
+
+function App() {
+  return (
+    <div>
+      <FaceExpressionDetector />
+    </div>
+  );
+}
+
+export default App;
